33ac132b2fad1e71bde85bceefb7bd9e69732f8f
Dell <2307vivek@gmail.com>
2021-02-28 17:24:31 +0530

    fix import in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
@@ -17,6 +17,7 @@ package com.example.androiddevchallenge
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.androiddevchallenge.ui.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
