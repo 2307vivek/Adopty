9a997eca1cc1e82c8642524cf504afd73b5dee55
Dell <2307vivek@gmail.com>
2021-08-05 15:28:10 +0530

    bug fixes.

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/home/HomeScreen.kt
@@ -60,6 +60,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -183,6 +184,15 @@ fun DogBreedContent(
                     modifier = Modifier
                         .padding(16.dp)
                         .fillMaxWidth()
+                )
+            }
+        } else if (pets.error != null) {
+            item { 
+                Text(
+                    text = pets.error,
+                    modifier = Modifier.padding(16.dp)
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center
                 )
             }
         }
